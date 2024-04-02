facdad4253eb9222eca6729f2e565f99e0d3f77a
dev <swami.rakesh2307@gmail.com>
2024-04-02 12:44:59 +0530

    more notes

--- src/behavior/a_adaptor/Client.java
+++ src/behavior/a_adaptor/Client.java
@@ -2,6 +2,7 @@ package behavior.a_adaptor;
 
 public class Client {
 
+
     public static void main(String[] args) {
 
         TemperatureAdaptorImpl temperatureAdaptor = new TemperatureAdaptorImpl(new TemperatureSensor());
